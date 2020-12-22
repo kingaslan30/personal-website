84bd59bb4aa5f1560cb486451ffe3676b2dfeb99
Arsalan Nasir <arsalan@arsalans-mbp.lan>
2020-12-22 16:55:46 -0600

    added contact page with mailchimp integration, also has basic required fields validation;

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -6,6 +6,7 @@ export default (props) => (
   <nav className="navigation"> 
     <Link to="/work">Work</Link>
     <Link to="/about">About</Link>
+    <Link to="/contact">Contact</Link>
     <ThemeChanger/>
   </nav>
   
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,70 @@ import React from "react"
 import Helmet from "react-helmet"
 import { graphql } from 'gatsby'
 import Layout from "../components/layout"
+import MailchimpSubscribe from "react-mailchimp-subscribe"
 
+
+const url = "https://app.us7.list-manage.com/subscribe/post?u=6158fad09bcb57c3ca73cfc8c&amp;id=8dba29128a";
+
+const CustomForm = ({ status, message, onValidated }) => {
+  let email, name, subject, msg;
+  const submit = () =>
+    email &&
+    subject &&
+    msg &&
+    name &&
+    email.value.indexOf("@") > -1 &&
+    onValidated({
+      EMAIL: email.value,
+      NAME: name.value,
+      SUBJECT: subject.value,
+      MESSAGE: msg.value
+    });
+
+  return (
+    <div className="two-grids -contact">
+      <div className="post-thumbnail" style={{backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`, marginBottom: 0}}>
+        <h1 className="post-title">Get in Touch</h1>
+        <p>Let me help you kick start your next project &rarr;</p>
+      </div>
+      <div className="form-container">
+        {status === "sending" && <div style={{ color: "blue" }}>sending...</div>}
+        {status === "error" && (
+          <div
+            style={{ color: "red" }}
+            dangerouslySetInnerHTML={{ __html: message }}
+          />
+        )}
+        {status === "success" && (
+          <div
+            style={{ color: "green" }}
+            dangerouslySetInnerHTML={{ __html: message }}
+          />
+        )}
+        <br></br>
+        <div>
+          <label htmlFor="w3lName">Name</label>
+          <input type="text" name="MMERGE3" id="w3lName" ref={node => (name = node)}/>
+        </div>
+        <div>
+          <label htmlFor="w3lSender">Email</label>
+          <input type="email" name="EMAIL" id="w3lSender" ref={node => (email = node)}/>
+        </div>
+        <div>
+          <label htmlFor="w3lSubject">Subject</label>
+          <input type="text" name="MMERGE4" id="w3lSubject" ref={node => (subject = node)}/>
+        </div>
+        <div>
+          <label htmlFor="w3lMessage">Message</label>
+          <textarea name="MMERGE5" id="w3lMessage" ref={node => (msg = node)}></textarea>
+        </div>
+        <div style={{display: "flex", justifyContent: "flex-end"}}>
+          <input type="submit" onClick ={submit} className="button -primary" style={{marginRight: 0}} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const ContactPage = ({
   data: {
@@ -15,35 +78,16 @@ const ContactPage = ({
         <title>Contact — {site.siteMetadata.title}</title>
         <meta name="description" content={"Contact page of " + site.siteMetadata.description} />
       </Helmet>
-      <div className="two-grids -contact">
-        <div className="post-thumbnail" style={{backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`, marginBottom: 0}}>
-          <h1 className="post-title">Get in Touch</h1>
-          <p>Let me help you kick start your next project &rarr;</p>
-        </div>
-        <div>
-          <form className="form-container" action="https://app.us7.list-manage.com/subscribe/post?u=6158fad09bcb57c3ca73cfc8c&amp;id=8dba29128a" method="post">
-            <div>
-              <label htmlFor="w3lName">Name</label>
-              <input type="text" name="MMERGE3" id="w3lName"/>
-            </div>
-            <div>
-              <label htmlFor="w3lSender">Email</label>
-              <input type="email" name="EMAIL" id="w3lSender"/>
-            </div>
-            <div>
-              <label htmlFor="w3lSubject">Subject</label>
-              <input type="text" name="MMERGE4" id="w3lSubject"/>
-            </div>
-            <div>
-              <label htmlFor="w3lMessage">Message</label>
-              <textarea name="MMERGE5" id="w3lMessage"></textarea>
-            </div>
-            <div style={{display: "flex", justifyContent: "flex-end"}}>
-              <input type="submit" className="button -primary" style={{marginRight: 0}} />
-            </div>
-          </form>
-        </div>
-      </div>
+      <MailchimpSubscribe
+        url={url}
+        render={({ subscribe, status, message }) => (
+          <CustomForm
+            status={status}
+            message={message}
+            onValidated={formData => subscribe(formData)}
+          />
+        )}
+      />
     </Layout>
   )
 }
